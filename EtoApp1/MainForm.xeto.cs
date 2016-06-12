4b1b7ff0afce0ebaaed2b1a54899a3ff21caea07
hal1932 <yu.arai.19@gmail.com>
2016-06-13 00:15:49 +0900

    initial commit

---
+++ EtoApp1/MainForm.xeto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+
+namespace EtoApp1
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            XamlReader.Load(this);
+            DataContext = new MainFormViewModel();
+        }
+
+        protected void HandleClickMe(object sender, EventArgs e)
+        {
+            MessageBox.Show("I was clicked!");
+            (DataContext as MainFormViewModel).TestLabel = "bbb";
+        }
+
+        protected void HandleQuit(object sender, EventArgs e)
+        {
+            Application.Instance.Quit();
+        }
+    }
+}
---
+++ EtoApp1/MainFormViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtoApp1
+{
+    class MainFormViewModel : INotifyPropertyChanged
+    {
+        private string _testLabel = "test";
+        public string TestLabel
+        {
+            get { return _testLabel; }
+            set { _testLabel = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string memberName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(memberName));
+        }
+    }
+}
